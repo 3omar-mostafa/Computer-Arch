82193bd75ada1540ae0ab0ee595ab41d286ed8f0
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2021-05-31 04:33:19 +0200

    Fixed IR Decoder

--- Decode_Stage/IR_Decoder.vhd
+++ Decode_Stage/IR_Decoder.vhd
@@ -4,11 +4,11 @@ USE ieee.numeric_std.ALL;
 
 ENTITY IR_DECODER IS
     PORT (
-        CLK, RESET : IN STD_LOGIC;
-        IR : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+        CLK, RESET            : IN STD_LOGIC;
+        IR                    : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
         R_SRC_NUM, R_DEST_NUM : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
-        OPCODE : OUT STD_LOGIC_VECTOR (4 DOWNTO 0);
-        IMMEDIATE : OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
+        OPCODE                : OUT STD_LOGIC_VECTOR (4 DOWNTO 0);
+        IMMEDIATE             : OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
     );
 END IR_DECODER;
 
@@ -21,17 +21,17 @@ BEGIN
 
         IF RESET = '1' THEN
 
-            R_SRC_NUM <= (OTHERS => '0');
+            R_SRC_NUM  <= (OTHERS => '0');
             R_DEST_NUM <= (OTHERS => '0');
-            OPCODE <= (OTHERS => '0');
-            IMMEDIATE <= (OTHERS => '0');
+            OPCODE     <= (OTHERS => '0');
+            IMMEDIATE  <= (OTHERS => '0');
 
         ELSIF RISING_EDGE(CLK) THEN
 
-            R_SRC_NUM <= IR(18 DOWNTO 16);
-            R_DEST_NUM <= IR(21 DOWNTO 19);
-            OPCODE <= IR(26 DOWNTO 22);
-            IMMEDIATE <= IR(15 DOWNTO 0);
+            R_DEST_NUM <= IR(19 DOWNTO 17);
+            R_SRC_NUM  <= IR(22 DOWNTO 20);
+            OPCODE     <= IR(27 DOWNTO 23);
+            IMMEDIATE  <= IR(15 DOWNTO 0);
 
         END IF;
     END PROCESS;
